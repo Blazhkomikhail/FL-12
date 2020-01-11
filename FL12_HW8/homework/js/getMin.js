e281e68a5c5ffdc547e4e153452c6c11de430148
Mykhayl Blazhko <michelin8585@gmail.com>
2020-01-11 15:08:20 +0200

    HW8/4

--- FL12_HW8/homework/js/getMin.js
+++ FL12_HW8/homework/js/getMin.js
@@ -1,6 +1,6 @@
 function getMin() {
 	let smaller = arguments[0];
-	for (let i = 0; i < 3; i++) {
+	for (let i = 1; i < 3; i++) {
 		if (arguments[i] < smaller) {
 			smaller = arguments[i];
 		}
--- FL12_HW8/homework/js/makeNumber.js
+++ FL12_HW8/homework/js/makeNumber.js
@@ -1,6 +1,5 @@
 function makeNumber (str) {
 	let readyStr = '';
-
 	for (let i = 0; i < str.length; i++) {
 		if ( isFinite(str[i]) ) {
 			readyStr += str[i];
